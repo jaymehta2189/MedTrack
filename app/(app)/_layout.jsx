1328e8069acf94f754403cfff7d3366d4b07ecfd
jaymehta2189 <jaymehta249@gmail.com>
2025-02-23 04:23:16 +0530

    added frontend

---
+++ app/(app)/_layout.jsx
@@ -0,0 +1,55 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function AppLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          borderTopWidth: 1,
+          borderTopColor: '#e0e0e0',
+        },
+        tabBarActiveTintColor: '#192f6a',
+        tabBarInactiveTintColor: '#a0a0a0',
+      
+      }}>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="AddMedicine"
+        options={{
+          title: 'Add Medicine',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Orders"
+        options={{
+          title: 'Orders',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="cart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
